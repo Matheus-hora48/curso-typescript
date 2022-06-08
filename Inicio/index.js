e11657c18bfc22c50fa6a7eec57889c30f7308bf
Matheus-hora48 <matheus11hora@gmail.com>
2022-06-08 17:41:19 -0300

    Novos

---
+++ Inicio/index.js
@@ -0,0 +1,4 @@
+function great(name) {
+    return "Ol\u00E1 mundo, voc\u00EA \u00E9 o ".concat(name, "?");
+}
+console.log(great('Matheus Hora'));
---
+++ Inicio/index.ts
@@ -0,0 +1,5 @@
+function  great(name){
+  return `Olá mundo, você é o ${name}?`
+}
+
+console.log(great('Matheus Hora'))
