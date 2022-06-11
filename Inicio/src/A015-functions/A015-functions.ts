9e2f4643a99369fa6c775430cc9ed52f52bbe785
Matheus-hora48 <matheus11hora@gmail.com>
2022-06-11 11:00:34 -0300

    Novos

---
+++ Inicio/src/A015-functions/A015-functions.ts
@@ -0,0 +1,16 @@
+type mapStingsCallback = (item: string) => string;
+
+function mapStings(array: string[], callbackfn: CallableFunction): string[] {
+  const newArray: string[] = [];
+
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(callbackfn(array[i]));
+  }
+
+  return newArray;
+}
+
+const abc = ['a', 'b', 'c'];
+const abcMapped = mapStings(abc, (item) => item.toUpperCase());
+
+console.log(abcMapped);
