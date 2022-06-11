cb9285f66bf93f84e71d1a40e4de76cea56d2aff
Matheus-hora48 <matheus11hora@gmail.com>
2022-06-11 11:47:30 -0300

    Novos

---
+++ Inicio/src/A017-type-assertions/A017-type-assertions.ts
@@ -0,0 +1,12 @@
+// condicional - quando nao tem certeza
+const body1 = document.querySelector('body');
+if (body1) body1.style.background = 'red';
+
+// assertion - quando tem certeza
+const body2 = document.querySelector('body') as HTMLBodyElement;
+body2.style.background = 'red';
+
+//htmlElemente
+const input = document.querySelector('.input') as HTMLBodyElement;
+input.value = 'Qualquer coisa';
+input.focus;
